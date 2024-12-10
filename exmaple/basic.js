ad0c7a76e50c4cc1df37131d5513872dbb633e34
tommy.chan.tek <tommy.chan.tek@axa.com.hk>
2024-12-10 17:53:00 +0800

    Add example

---
+++ exmaple/basic.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+require('Documents/phoenix-config/dist/PhoenixTWM.js');
+
+// PhoenixTWM.setActiveBorderColor('0xffe1e3e4');
+// PhoenixTWM.setInactiveBorderColor('0xff494d64');
+// PhoenixTWM.setBorderWidth(5);
+// PhoenixTWM.enableJankyBorders();
+
+// PhoenixTWM.setGap(10);
+
+const CTRL = ['ctrl'];
+const CTRL_OPT = ['ctrl', 'option'];
+
+Key.on('r', CTRL_OPT, () => {
+  // console.log(PhoenixTWM.refreshCurrentScreenLayout);
+  PhoenixTWM.refreshCurrentScreenLayout();
+});
+
+Key.on('s', CTRL_OPT, () => {
+  PhoenixTWM.moveFocusedWindowForward();
+});
+
+Key.on('t', CTRL_OPT, () => {
+  PhoenixTWM.moveFocusedWindowBackward();
+});
